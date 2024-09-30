0dbb1ebe19b0fdbe5b122cbe4b15908753de8d05
Lugi-alo <ejolohan@gmail.com>
2024-09-30 23:00:36 +0100

    updated rotor class functions

--- rotor.py
+++ rotor.py
@@ -3,6 +3,21 @@ class Rotor:
     def __init__(self, wiring, notch):
         self.wiring = wiring
         self.notch = notch
-        
+        self.position = 0
 
+    def rotate(self):
+        self.position = (self.position + 1) % 26
+        return self.wiring[self.position] == self.notch
+
+    def forward(self, letter):
+        number = ord(letter) - 97
+        rearrangedNumber = (number + self.position) % 26
+        return chr(rearrangedNumber + 97)
+        
+    def backward(self, letter):
+        number = self.wiring.index(letter)
+        rearrangedNumber = (number - self.position) % 26
+        return chr(rearrangedNumber + 97)
+        
+    
 
