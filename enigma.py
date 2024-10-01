2e17d790a8a2b405444b3cf08bcf2ae1e4c86db6
Lugi-alo <ejolohan@gmail.com>
2024-10-01 15:24:00 +0100

    initalised enigma class and updated plugboard class

--- enigma.py
+++ enigma.py
@@ -1,13 +1,44 @@
+from rotor import Rotor
+from reflector import Reflector
+from plugboard import Plugboard
+
 class Enigma:
+
     def __init__(self):
+        self.rotors = []
         self.setup()
 
     def setup(self):
+
+        #initialise the first rotor
         rotorWiringI = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
         rotorNotchI = 'Q'
-        
+
+        reflectorInstanceI = Reflector(rotorWiringI)
+        plugboardInstanceI = Plugboard(rotorWiringI)
+
+        rotorInstanceI = Rotor(rotorWiringI, rotorNotchI)
+        self.rotors.append(rotorInstanceI)
+
+        #initialise the second rotor
         rotorWiringII = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
         rotorNotchII = 'E'
         
+        reflectorInstanceII = Reflector(rotorWiringII)
+        plugboardInstanceII = Plugboard(rotorWiringII)
+
+        rotorInstanceII = Rotor(rotorWiringII, rotorNotchII)
+        self.rotors.append(rotorInstanceII)
+        
+        #initialise the third rotor
         rotorWiringIII = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
         rotorNotchIII = 'V'
+
+        reflectorInstanceIII = Reflector(rotorWiringII)
+        plugboardInstanceIII = Plugboard(rotorWiringIII)
+
+        rotorInstanceIII = Rotor(rotorWiringIII, rotorNotchIII)
+        self.rotors.append(rotorInstanceIII)
+
+        return [rotorInstanceI, rotorInstanceII, rotorInstanceIII]
+
--- plugboard.py
+++ plugboard.py
@@ -1,3 +1,10 @@
 class Plugboard:
+    
     def __init__(self, wiring):
         self.wiring = wiring
+
+    def swap(self, letter):
+        return self.wiring.get(letter, letter)
+        
+        
+
