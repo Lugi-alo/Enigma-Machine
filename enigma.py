aaabb9937889ebd967a8686a3e834208c170acf7
Lugi-alo <ejolohan@gmail.com>
2024-10-09 15:47:40 +0100

    added encryption, rotor rotation and run function to enigma class

--- enigma.py
+++ enigma.py
@@ -40,6 +40,58 @@ class Enigma:
         plugboardInstance = Plugboard(swaps)
         self.plugboard.append(plugboardInstance)
 
+    def rotorRotation(self):
+        
+        # Rotate the first rotor
+        rotate_next = self.rotors[0].rotate()
+
+        # Handle double-stepping mechanism
+        if rotate_next:
+            if len(self.rotors) > 1:
+                rotate_next = self.rotors[1].rotate()
+                if rotate_next and len(self.rotors) > 2:
+                    self.rotors[2].rotate()
+
+    def encryption(self, letter):
+        
+        self.rotorRotation()
+        print(f"\nEncrypting letter: {letter}")
+
+        # Plugboard swap before rotors 
+        letter = self.plugboard[0].swap(letter)
+        print(f"After plugboard (Rotor 1): {letter}")
+
+        # Forward pass through rotors
+        for i, rotor in enumerate(self.rotors):
+            letter = rotor.forward(letter)
+            print(f"After rotor {i+1} forward: {letter}")
+
+        # Reflector
+        letter = self.reflector.reflect(letter)
+        print("After reflector: " + letter )
+
+        # Backward pass through rotors
+        for i, rotor in enumerate(reversed(self.rotors)):
+            letter = rotor.backward(letter)
+            print("After rotor " + str(len(self.rotors)-i) + "backward: " + letter)
+
+        # Use the plugboard corresponding to the last rotor after encrypting
+        letter = self.plugboard[-1].swap(letter)
+        print("Final encrypted letter:  " + letter)
+
+        return letter
+
+    def run(self):
+        userInput = input("Enter your message: ").upper()
+        encryptedOutput = ""
+
+        for letter in userInput:
+            if letter in alphabet:
+                encryptedOutput = encryptedOutput + self.encryption(letter)
+            else:
+                encryptedOutput = encrpytedOutput + letter  
+
+        print("\nEncrypted message: " + encryptedOutput)
 
 # Main function
 if __name__ == "__main__":
--- plugboard.py
+++ plugboard.py
@@ -4,16 +4,16 @@ alphabet = string.ascii_uppercase
 
 class Plugboard:
     
-    def __init__(self, swap=None):
+    def __init__(self, swaps=None):
         self.mapping = {}
 
-        if swap is None:
-            swap = []
+        if swaps is None:
+            swaps = []
             
         for letter in alphabet:
             self.mapping[letter] = letter
             
-        for a, b in swap:
+        for a, b in swaps:
             self.mapping[a] = b
             self.mapping[b] = a
 
